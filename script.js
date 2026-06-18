6cdcf473f79cbdc3dbd35b1a12ea15ab094008a9
thejasdenni <thejasdenni@gmail.com>
2026-06-18 12:12:53 +0530

    Added feature to display Date below the selector

--- script.js
+++ script.js
@@ -9,7 +9,14 @@ week.addEventListener("change", function(){
     const mondayOffSet = (dayOfWeek === 0 ? 6 : dayOfWeek - 1);
     const monday = new Date(jan4);
     monday.setDate(jan4.getDate() - mondayOffSet + (weekNum - 1) * 7);
-    console.log(monday);
+    const saturday = new Date(monday);
+    saturday.setDate(monday.getDate() + 5);
+    const options1 = { month: 'long', day: 'numeric'};
+    const options2 = {day: 'numeric', year: 'numeric' };
+    const dateRange = monday.toLocaleDateString('en-US',options1) + ' - ' + saturday.toLocaleDateString('en-US',options2);
+    console.log("Date is " + dateRange);
+        const paragraph = document.getElementById('weekRange');
+        paragraph.innerText = dateRange;
 }
 );
 
